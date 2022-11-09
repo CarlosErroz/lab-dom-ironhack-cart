ab96a167c87c5d461c039f5781c202c120ce74fa
Carlos Erroz <carlos.erroz@hotmail.com>
2022-11-09 18:40:42 +0100

    Iteration 2 hecha

--- js/index.js
+++ js/index.js
@@ -1,20 +1,26 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
+  //... your code goes here
   const price = product.querySelector(".price span").innerHTML;
   const quantity = product.querySelector(".quantity input").value;
   const subtotalPrice = price * quantity;
   const resultSubtotal = product.querySelector(".subtotal span");
   return (resultSubtotal.innerHTML = subtotalPrice);
-  //... your code goes here
+  
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
+  const productsAll = document.getElementsByClassName("product");
+  for (let i=0;i<productsAll.length;i++) {
+  updateSubtotal(productsAll[i]);
+  }
+
 
 // js/index.js
 
